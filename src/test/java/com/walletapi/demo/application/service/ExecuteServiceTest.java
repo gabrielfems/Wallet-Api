9141e5422e88c13f550013c680670d3088e1f4a5
devgabs1 <gabriel.fe132@gmail.com>
2026-04-23 19:46:36 -0300

    Finalizando testes unitários: ExecuteServiceTest

--- src/test/java/com/walletapi/demo/application/service/ExecuteServiceTest.java
+++ src/test/java/com/walletapi/demo/application/service/ExecuteServiceTest.java
@@ -1,25 +1,33 @@
 package com.walletapi.demo.application.service;
 
+import com.walletapi.demo.application.dto.TransactionDepositDTO;
+import com.walletapi.demo.application.dto.TransactionWithdrawDTO;
 import com.walletapi.demo.application.dto.UserCreateDTO;
 import com.walletapi.demo.application.exceptions.InsufficientBalanceException;
 import com.walletapi.demo.domain.entities.User;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 
 class ExecuteServiceTest {
 
     private ExecuteService executeService;
+
+    private Validator validator;
 
     @InjectMocks
     private TransactionValidatorService transactionValidatorService;
@@ -27,30 +35,34 @@ class ExecuteServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
         executeService = new ExecuteService();
     }
 
     @Test
     @DisplayName("Should execute a transfer when everything is OK")
     void executeTransferCase1() {
-                UserCreateDTO sender = new UserCreateDTO("Gabriel",
-                "gabriel.fe132@gmail.com",
+        UserCreateDTO sender = new UserCreateDTO("Gabriel",
+                "gabriel@gmail.com",
                 "123@pass",
                 "44974002293",
-                "87080283",
+                "87080078",
                 "123",
                 "b",
-                LocalDate.of(2002, 02, 24),
+                LocalDate.of(2002, 12, 20),
                 "1234567890");
 
         UserCreateDTO receiver = new UserCreateDTO("Gabriel",
-                "gabriel.fe132@gmail.com",
+                "gabriel@gmail.com",
                 "123@pass",
                 "44974002293",
-                "87080283",
+                "87080078",
                 "123",
                 "b",
-                LocalDate.of(2002, 02, 24),
+                LocalDate.of(2002, 12, 20),
                 "1234567890");
 
         User senderUser = new User(sender);
@@ -74,23 +86,23 @@ class ExecuteServiceTest {
     @DisplayName("Should throw exception when sender balance is insufficient")
     void executeTransferCase2() {
         UserCreateDTO sender = new UserCreateDTO("Gabriel",
-                "gabriel.fe132@gmail.com",
+                "gabriel@gmail.com",
                 "123@pass",
                 "44974002293",
-                "87080283",
+                "87080078",
                 "123",
                 "b",
-                LocalDate.of(2002, 02, 24),
+                LocalDate.of(2002, 12, 20),
                 "1234567890");
 
         UserCreateDTO receiver = new UserCreateDTO("Gabriel",
-                "gabriel.fe132@gmail.com",
+                "gabriel@gmail.com",
                 "123@pass",
                 "44974002293",
-                "87080283",
+                "87080078",
                 "123",
                 "b",
-                LocalDate.of(2002, 02, 24),
+                LocalDate.of(2002, 12, 20),
                 "1234567890");
 
         User senderUser = new User(sender);
@@ -116,10 +128,152 @@ class ExecuteServiceTest {
     }
 
     @Test
-    void executeDeposit() {
+    @DisplayName("Should execute the deposit when everything is OK")
+    void executeDepositCase1() {
+        UserCreateDTO sender = new UserCreateDTO("Gabriel",
+                "gabriel.fe132@gmail.com",
+                "123@pass",
+                "44974002293",
+                "87080283",
+                "123",
+                "b",
+                LocalDate.of(2002, 02, 24),
+                "1234567890");
+
+        User createUser = new User(sender);
+        createUser.setId(1L);
+        createUser.getWallet().setBalance(BigDecimal.valueOf(100));
+
+        executeService.executeDeposit(createUser, BigDecimal.valueOf(100));
+
+        assertThat(createUser.getWallet().getBalance())
+        .isEqualByComparingTo("200");
+
     }
 
     @Test
-    void executeWithdraw() {
+    @DisplayName("Should thrown a exception when amount is NULL")
+    void executeDepositCase2() {
+        TransactionDepositDTO dto = new TransactionDepositDTO(null, 1L);
+
+        Set<ConstraintViolation<TransactionDepositDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("amount", "Valor obrigatório")
+                );
     }
+
+    @Test
+    @DisplayName("Should thrown a exception when amount is NEGATIVE")
+    void executeDepositCase3() {
+        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(-1), 1L);
+
+        Set<ConstraintViolation<TransactionDepositDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("amount", "Valor deve ser positivo")
+                );
+    }
+
+    @Test
+    @DisplayName("Should thrown a exception when user is EMPTY")
+    void executeDepositCase4() {
+        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(10), null);
+
+        Set<ConstraintViolation<TransactionDepositDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("userId", "Usuário obrigatório")
+                );
+    }
+
+    @Test
+    @DisplayName("Should execute the withdraw when everything is OK")
+    void executeWithdrawCase1() {
+        UserCreateDTO sender = new UserCreateDTO("Gabriel",
+                "gabriel@gmail.com",
+                "123@pass",
+                "44974002293",
+                "87080078",
+                "123",
+                "b",
+                LocalDate.of(2002, 12, 20),
+                "1234567890");
+
+        User createUser = new User(sender);
+        createUser.setId(1L);
+        createUser.getWallet().setBalance(BigDecimal.valueOf(100));
+
+        executeService.executeWithdraw(createUser, BigDecimal.valueOf(100));
+
+        assertThat(createUser.getWallet().getBalance())
+                .isEqualByComparingTo("0");
+    }
+
+    @Test
+    @DisplayName("Should thrown a exception when amount is NULL")
+    void executeWithdrawCase2() {
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(null, 1L);
+
+        Set<ConstraintViolation<TransactionWithdrawDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("amount", "Valor obrigatório")
+                );
+    }
+
+    @Test
+    @DisplayName("Should thrown a exception when amount is NEGATIVE")
+    void executeWithdrawCase3() {
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(-1), 1L);
+
+        Set<ConstraintViolation<TransactionWithdrawDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("amount", "Valor deve ser positivo")
+                );
+    }
+
+    @Test
+    @DisplayName("Should thrown a exception when user is EMPTY")
+    void executeWithdrawCase4() {
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(10), null);
+
+        Set<ConstraintViolation<TransactionWithdrawDTO>> violations = validator.validate(dto);
+
+        assertThat(violations)
+                .extracting(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )
+                .containsExactly(
+                        tuple("senderId", "Remetente obrigatório")
+                );
+    }
+
 }
