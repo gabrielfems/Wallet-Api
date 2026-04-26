5cd46ad84e9f260e95b894717d4bbf367467410a
devgabs1 <gabriel.fe132@gmail.com>
2026-04-26 17:30:06 -0300

    Finalizando testes de TransactionServiceTest

--- src/test/java/com/walletapi/demo/application/service/TransactionServiceTest.java
+++ src/test/java/com/walletapi/demo/application/service/TransactionServiceTest.java
@@ -3,7 +3,6 @@ package com.walletapi.demo.application.service;
 import com.walletapi.demo.application.dto.TransactionDepositDTO;
 import com.walletapi.demo.application.dto.TransactionTransferDTO;
 import com.walletapi.demo.application.dto.TransactionWithdrawDTO;
-import com.walletapi.demo.application.dto.UserCreateDTO;
 import com.walletapi.demo.application.exceptions.InsufficientBalanceException;
 import com.walletapi.demo.application.exceptions.ReceiverUserNotFoundException;
 import com.walletapi.demo.application.exceptions.SenderUserNotFoundException;
@@ -11,7 +10,6 @@ import com.walletapi.demo.domain.entities.Transaction;
 import com.walletapi.demo.domain.entities.User;
 import com.walletapi.demo.domain.enums.TransactionType;
 import com.walletapi.demo.infrastructure.repositories.TransactionRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,11 +18,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,29 +44,10 @@ class TransactionServiceTest {
     private User userSender;
     private User userReceiver;
 
-    @BeforeEach
-    void setUp() {
-        UserCreateDTO dto1 = new UserCreateDTO(
-                "Gabriel", "gabriel@gmail.com", "123@pass",
-                "44974002293", "87080078", "123", "b",
-                LocalDate.of(2002, 12, 20), "1234567890");
-
-        UserCreateDTO dto2 = new UserCreateDTO(
-                "José", "ze@gmail.com", "123@pass",
-                "44974002323", "87009078", "133", "a",
-                LocalDate.of(2003, 2, 26), "1234567890");
-
-        userSender = new User(dto1);
-        userSender.setId(1L);
-
-        userReceiver = new User(dto2);
-        userReceiver.setId(2L);
-    }
-
     @Test
     @DisplayName("Should create the transfer when everything is ok")
     void createTransferCase1() {
-        TransactionTransferDTO dto = new TransactionTransferDTO( BigDecimal.valueOf(150),1L, 2L);
+        TransactionTransferDTO dto = new TransactionTransferDTO( BigDecimal.valueOf(150),userSender.getId(), userReceiver.getId());
 
         when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
         when(transactionValidatorService.validateReceiver(2L)).thenReturn(userReceiver);
@@ -90,7 +67,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should throw an exception because balance is insufficient")
     void createTransferCase2() {
-        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), 1L, 2L);
+        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), userSender.getId(),userReceiver.getId());
 
         when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
         when(transactionValidatorService.validateReceiver(2L)).thenReturn(userReceiver);
@@ -104,7 +81,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should throw an exception when sender is not found")
     void createTransferCase3() {
-        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), 1L, 2L);
+        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), userSender.getId(),userReceiver.getId());
 
         doThrow(new SenderUserNotFoundException())
                 .when(transactionValidatorService).validateSender(1L);
@@ -116,7 +93,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should throw an exception when receiver is not found")
     void createTransferCase4() {
-        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), 1L, 2L);
+        TransactionTransferDTO dto = new TransactionTransferDTO(BigDecimal.valueOf(150), userSender.getId(), userReceiver.getId());
 
         when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
         doThrow(new ReceiverUserNotFoundException())
@@ -129,7 +106,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should create the deposit when everything is ok")
     void createDepositCase1() {
-        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(150),1L);
+        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(150),userSender.getId());
 
         when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
 
@@ -148,7 +125,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should throw an exception when sender is not found")
     void createDepositCase2() {
-        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(150), 1L);
+        TransactionDepositDTO dto = new TransactionDepositDTO(BigDecimal.valueOf(150), userSender.getId());
 
         doThrow(new SenderUserNotFoundException())
                 .when(transactionValidatorService).validateSender(1L);
@@ -160,7 +137,7 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should create the withdraw when everything is ok")
     void createWithdrawCase1() {
-        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(150),1L);
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(150),userSender.getId());
 
         when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
 
@@ -175,5 +152,32 @@ class TransactionServiceTest {
         verify(executeService, times(1)).executeWithdraw(userSender, dto.amount());
         verify(transactionValidatorService, times(1)).validateWithdraw(userSender, dto.amount());
         verify(transactionRepository).save(any(Transaction.class));
+    }
+
+    @Test
+    @DisplayName("Should throw an exception when balance is insufficient")
+    void createWithdrawCase2() {
+
+        when(transactionValidatorService.validateSender(1L)).thenReturn(userSender);
+
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(150), userSender.getId());
+
+        doThrow(new InsufficientBalanceException())
+                .when(transactionValidatorService).validateWithdraw(userSender,BigDecimal.valueOf(150));
+
+        assertThatThrownBy(() -> transactionService.createWithdraw(dto))
+                .isInstanceOf(InsufficientBalanceException.class);
+    }
+
+    @Test
+    @DisplayName("Should throw an exception when sender is not found")
+    void createWithdrawCase3() {
+        TransactionWithdrawDTO dto = new TransactionWithdrawDTO(BigDecimal.valueOf(150), userSender.getId());
+
+        doThrow(new SenderUserNotFoundException())
+                .when(transactionValidatorService).validateSender(userSender.getId());
+
+        assertThatThrownBy(() -> transactionService.createWithdraw(dto))
+                .isInstanceOf(SenderUserNotFoundException.class);
     }
 }
