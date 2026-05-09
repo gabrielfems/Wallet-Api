b93e8a222aa74d49a20b42b876f955df4ff4b03d
devgabs1 <gabriel.fe132@gmail.com>
2026-05-09 17:56:02 -0300

    Implementando classe com dados de login dos usuários

---
+++ src/main/java/com/walletapi/demo/domain/entities/UserCredentials.java
@@ -0,0 +1,71 @@
+package com.walletapi.demo.domain.entities;
+
+import com.walletapi.demo.domain.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Table(name= "user_credentials")
+@Entity(name = "user_credentials")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class UserCredentials implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String login;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public UserCredentials(String login, String password, UserRole role){
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
